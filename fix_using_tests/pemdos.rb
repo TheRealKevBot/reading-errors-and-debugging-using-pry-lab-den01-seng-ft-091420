20e1247a431c23d770623e4fff50c5b274b9b7cc
Kevin Glasgow <kglas001@gmail.com>
2020-09-01 21:08:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,8 +4,10 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    new_string = 10.times {"s"}
-    p 
+    binding.pry
+    10.times {"s"} string
+    binding.pry 
+    p new_string
   else
     string
   end
